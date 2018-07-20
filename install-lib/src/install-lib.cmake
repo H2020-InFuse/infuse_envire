3979f2e1845abaecfe040a284e447516ff407611
Pierre Narvor <pnarvor@laas.fr>
2018-07-20 15:16:38 +0200

    [install-lib] upgraded install lib for cmake subdirectories

--- install-lib/src/install-lib.cmake
+++ install-lib/src/install-lib.cmake
@@ -5,11 +5,11 @@ install(CODE "
     set(PROJECT_VERSION ${PROJECT_VERSION})
     set(DEPENDENCIES \"${DEPENDENCIES}\")
     foreach(header ${HEADERS})
-        file(RELATIVE_PATH rpath ${CMAKE_SOURCE_DIR}/src ${CMAKE_SOURCE_DIR}/\${header})
+        file(RELATIVE_PATH rpath ${CMAKE_CURRENT_SOURCE_DIR}/src ${CMAKE_CURRENT_SOURCE_DIR}/\${header})
         get_filename_component(rpath \${rpath} DIRECTORY)
         file(INSTALL ${CMAKE_CURRENT_SOURCE_DIR}/\${header} DESTINATION ${CMAKE_INSTALL_PREFIX}/include/${LIBRARY_NAME}/\${rpath})
     endforeach()
-    message(STATUS \"Installing ${CMAKE_INSTALL_PREFIX}/lib/pkgconfig/${LIBRARY_NAME}.pc\")
+    message(STATUS \"Installing: ${CMAKE_INSTALL_PREFIX}/lib/pkgconfig/${LIBRARY_NAME}.pc\")
     configure_file(${CMAKE_CURRENT_LIST_DIR}/template.pc.in ${CMAKE_INSTALL_PREFIX}/lib/pkgconfig/${LIBRARY_NAME}.pc @ONLY)")
 ## Mark executables and/or libraries for installation
 install(TARGETS ${LIBRARY_NAME}
